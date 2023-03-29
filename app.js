65091f34289f3a39b1559e3f7f2a7c7934012897
张湾 <zhangxiaopeng@itdongfang.com>
2023-03-29 16:00:55 +0800

    frist

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-// 导入 express 模块 
-const express = require('express') 
-// 创建 express 的服务器实
-// 之后的其他配置都写在这里例 
-const app = express() 
-// 导入 cors 中间件 
-const cors = require('cors') 
-//设置public为静态文件的存放文件夹
-app.use('/public',express.static('public'));
-// 将 cors 注册为全局中间件，允许跨域请求 
-app.use(cors())
-app.use(express.urlencoded({ extended: false }))
-const userRouter = require('./router/user')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.use('/api', userRouter)
-// 调用 app.listen 方法，指定端口号并启动web服务器 
-app.listen(8888, function () {   
-console.log('server is running at http://127.0.0.1:8888') 
-})
-// 导入配置文件
-const config = require('./config')
-// 解析 token 的中间件
-const expressJWT = require('express-jwt')
-// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证
-app.use(expressJWT({ secret: config.jwtSecretKey }).unless({ path: [/^\/login\//] }))
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var cors = require('cors') 
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
